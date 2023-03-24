210d03982ba0112b0310d462d827e344d73aff65
muneebrana414 <muneebrana414@gmail.com>
2023-03-24 16:23:19 +0500

    setup react app frontend

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="sidebar">
+        <>
+          <b>Connected as:</b>
+          <br />
+        </>
+        <button>
+          Connect With MetaMask
+        </button>
+        <p>Please install MetaMask</p>
+      </div>
+      <div className="chat">
+        <div className="s">
+          <p className="state-message">
+            Connect to the chat in order to see the messages!
+          </p>
+          <p className="state-message">There is no message to display</p>
+        </div>
+        <div className="chat__actions-wrapper">
+          <p className="state-message">Connect With Metamask to chat!</p>
+          <div className="chat__input">
+            <textarea></textarea>
+            <button>
+              {"send message"}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
